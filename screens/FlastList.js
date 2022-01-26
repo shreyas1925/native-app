816bd89e5d8884fd2b5486ab39ab178daa947ee1
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-01-26 15:50:41 +0530

    flatlist unique key

--- screens/FlastList.js
+++ screens/FlastList.js
@@ -5,26 +5,32 @@ const FlastList = () => {
 
     const listOfnames = [
         {
+            index: "1",
             name: "Shreyas"
         },
         {
+            index: "2",
             name: "James"
         },
         {
+            index: "3",
             name: "John"
         },
         {
+            index: "4",
             name: "Marim"
         },
         {
+            index: "5",
             name: "Xoxo"
         },]
     return (
         <View>
             <Text style={styles.text}>List of names 👨‍💻</Text>
-            <FlatList data={listOfnames} renderItem={(ele) => {
-                return <Text style={styles.lists}>{ele.item.name}</Text>
-            }}
+            <FlatList keyExtractor={(key) => { return key.index }}
+                data={listOfnames} renderItem={(ele) => {
+                    return <Text style={styles.lists}>{ele.item.name}</Text>
+                }}
             />
         </View>
     )

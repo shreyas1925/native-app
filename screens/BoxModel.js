03436f17c5d9cfc3aa3c81625e9ee420b4a61dfb
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-03-04 10:43:40 +0530

    flex model

--- screens/BoxModel.js
+++ screens/BoxModel.js
@@ -14,3 +14,7 @@ export default BoxModel
 // 1. paddingVertical and paddingHorizontal in react native
 // 2. borderWidth , borderColor
 // 3. marginHorizontal, marginVertical
+// 4. In React native flex direction will be default column
+// 5. So justify works on vertical we can change it
+// 6. always use flexdirection,justifycontent etc on parent only
+// 7.flex:1, align-self can be use any child div

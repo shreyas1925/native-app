414ea78a5fce0705b56ada962d6e116d723fad92
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-03-04 17:33:12 +0530

    simple UI

--- Components/Counter.js
+++ Components/Counter.js
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+
 import React, { useState } from 'react'
 import { TouchableOpacity } from 'react-native-web'
 
@@ -6,28 +7,64 @@ const Counter = () => {
 
   const [count, setcount] = useState(0)
   return (
-    <View>
-      <Text>{count}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.textStyle}>
+        <Text>{count}</Text>
+      </View>
 
-      <TouchableOpacity
-        onPress={() => setcount(count + 5)}
-      >
-        <Text>Increment</Text>
-      </TouchableOpacity>
+      <View>
+        <TouchableOpacity
 
-      <TouchableOpacity
-        onPress={() => { count > 0 ? setcount(count - 5) : setcount(0) }}
-      >
-        <Text>Decrement</Text>
-      </TouchableOpacity>
+          style={styles.buttonStyle}
+          onPress={() => setcount(count + 5)}
+        >
+          <Text>Increment</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => { setcount(0) }}
-      >
-        <Text>Reset</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => { count > 0 ? setcount(count - 5) : setcount(0) }}
+        >
+          <Text>Decrement</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => { setcount(0) }}
+        >
+          <Text>Reset</Text>
+        </TouchableOpacity>
+
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+
+  mainContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: "100vh",
+  },
+  textStyle: {
+    borderWidth: 3,
+    padding: 15,
+    // alignSelf: "stretch",
+    width: "40%",
+    marginBottom: 30
+  },
+  buttonStyle: {
+    padding: 10,
+    borderWidth: 3,
+    margin: 30,
+    textAlign: "center",
+    width: "80%",
+
+  }
+
+})
+
+
 
 export default Counter
